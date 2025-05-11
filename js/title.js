3b2ee7d95cf6709493ef86879ec10d2b8d30ebcb
Wynphor <3135303781@qq.com>
2025-05-11 17:28:21 +0800

    Site updated: 2025-05-11 17:28:21

--- js/title.js
+++ js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '❤️⭐大爷，快来玩呀~⭐❤️|Wynphor の Nest';
+    document.title = '❤️⭐大爷，快来玩呀~⭐❤️ | Wynphor の Nest';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🤣性感咲楓，在线胡说~🤣|Wynphor の Nest';
+    document.title = '🤣性感咲楓，在线胡说~🤣 | Wynphor の Nest';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
